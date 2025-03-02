e2978a573b2efaedcba31c46cade56770d6847c4
turbojax07 <turbojax07@gmail.com>
2025-03-02 17:37:54 -0500

    Updating VisionFunctions.java

--- util/VisionFunctions.java
+++ util/VisionFunctions.java
@@ -1,16 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.vision.util;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.targeting.MultiTargetPNPResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.Matrix;
@@ -20,7 +8,13 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
-import frc.robot.subsystems.vision.VisionConstants;
+import frc.robot.Constants.VisionConstants;
+import java.util.List;
+import java.util.Optional;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.targeting.MultiTargetPNPResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 /** Add your docs here. */
 public class VisionFunctions {
@@ -31,34 +25,25 @@ public class VisionFunctions {
     }
 
     public static Matrix<N3, N1> getStdDevs(PhotonCamera camera, Pose2d estimatedPose, PhotonPoseEstimator poseEstimator) {
-        Matrix<N3, N1> estStdDevs = VisionConstants.singleTagStdDevs;
-        List<PhotonTrackedTarget> targets = camera.getLatestResult().targets;
+        List<PhotonTrackedTarget> targets = camera.getAllUnreadResults().get(0).getTargets();
         int numTags = 0;
         double avgDist = 0;
         for (PhotonTrackedTarget target : targets) {
             Optional<Pose3d> tagPose = poseEstimator.getFieldTags().getTagPose(target.fiducialId);
-            if (tagPose.isEmpty()) {
-                continue;
-            }
+            if (tagPose.isEmpty()) continue;
+
             numTags++;
             avgDist += tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
         }
 
-        if (numTags == 0) {
-            return estStdDevs;
-        }
+        if (numTags == 0) return VisionConstants.singleTagStdDevs;
+
+        if (numTags > 1) return VisionConstants.multiTagStdDevs;
 
         avgDist /= numTags;
-        if (numTags > 1) {
-            estStdDevs = VisionConstants.multiTagStdDevs;
-        }
 
-        if (numTags == 1 && avgDist > 4) {
-            estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
-        } else {
-            estStdDevs = estStdDevs.times(1 + ((avgDist*avgDist)/30));
-        }
-        
-        return estStdDevs;
+        if (numTags == 1 && avgDist > 4) return VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+
+        return VisionConstants.singleTagStdDevs.times(1 + (avgDist * avgDist / 30));
     }
 }
