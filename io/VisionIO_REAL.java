cb2c06dd478ef51d8962b4d05e6b0d257a2ba7fa
turbojax07 <turbojax07@gmail.com>
2025-03-02 17:37:30 -0500

    Updating IO getUnreadResults functions and formatting

--- io/VisionIO_REAL.java
+++ io/VisionIO_REAL.java
@@ -1,22 +1,21 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.vision.io;
 
-import frc.robot.subsystems.vision.VisionConstants;
-import java.util.List; 
-import java.util.Optional;
+import static frc.robot.subsystems.vision.util.VisionFunctions.getStdDevs;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.subsystems.vision.util.VisionResult;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.targeting.PhotonPipelineResult;
-
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.math.geometry.Pose2d;
-import frc.robot.subsystems.vision.util.VisionResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 /** Add your docs here. */
 public class VisionIO_REAL implements VisionIO {
@@ -29,11 +28,11 @@ public class VisionIO_REAL implements VisionIO {
         cameras = new PhotonCamera[VisionConstants.CameraIDs.length];
         poseEstimators = new PhotonPoseEstimator[cameras.length];
 
-        for (int i=0; i<cameras.length; i++) {
+        for (int i = 0; i < cameras.length; i++) {
             cameras[i] = new PhotonCamera(VisionConstants.CameraIDs[i]);
             poseEstimators[i] = new PhotonPoseEstimator(AprilTagFieldLayout.loadField(VisionConstants.field), VisionConstants.strategy, VisionConstants.CameraTransforms[i]);
         }
-
+        
         inputs = new VisionIOInputsAutoLogged();
     }
 
@@ -46,21 +45,39 @@ public class VisionIO_REAL implements VisionIO {
 
     @Override
     public VisionResult[] getUnreadResults() {
-        Logger.recordOutput("Cameras/Measuring", true);
-        VisionResult[] visionMeasurements = new VisionResult[cameras.length];
-        for (int i=0; i<cameras.length; i++) {
-            List<PhotonPipelineResult> results = cameras[i].getAllUnreadResults();
-            if (results.size() > 0) {
-                Optional<EstimatedRobotPose> estimatedPose = poseEstimators[i].update(results.get(0));
-                if (estimatedPose.isPresent()) {
-                    visionMeasurements[i] = new VisionResult(estimatedPose.get().estimatedPose, results.get(0).getTimestampSeconds());
+        ArrayList<VisionResult> results = new ArrayList<VisionResult>();
+
+        for (int i = 0; i < cameras.length; i++) {
+            PhotonCamera camera = cameras[i];
+            PhotonPoseEstimator poseEstimator = poseEstimators[i];
+
+            List<PhotonPipelineResult> pipelineResults = camera.getAllUnreadResults();
+            for (PhotonPipelineResult pipelineResult : pipelineResults) {
+                Optional<EstimatedRobotPose> pose = poseEstimator.update(pipelineResult);
+                
+                if (pose.isPresent()) {
+                    results.add(new VisionResult(pose.get().estimatedPose, pose.get().timestampSeconds, getStdDevs(camera, pose.get().estimatedPose.toPose2d(), poseEstimator)));
                 }
+
+                List<PhotonTrackedTarget> targets = pipelineResult.getTargets();
+                int[] targetIds = new int[targets.size()];
+                for (int j = 0; j < targets.size(); j++) {
+                    PhotonTrackedTarget target = targets.get(i);
+
+                    int id = target.getFiducialId();
+                    targetIds[i] = id;
+
+                    Logger.recordOutput(String.format("/Vision/Targets/Target%d/Rotation", id), new Rotation3d(target.getSkew(), target.getPitch(), target.getYaw()));
+                    Logger.recordOutput(String.format("/Vision/Targets/Target%d/Transform", id), target.getBestCameraToTarget());
+                }
+
+                Logger.recordOutput("/Vision/TrackedIDs", targetIds);
             }
         }
 
-        return visionMeasurements;
+        return (VisionResult[]) results.toArray();
     }
 
-	@Override
-	public void setRobotPose(Pose2d pose) {}
+    @Override
+    public void setRobotPose(Pose2d pose) {}
 }
--- io/VisionIO_SIM.java
+++ io/VisionIO_SIM.java
@@ -1,13 +1,14 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.vision.io;
 
-import frc.robot.subsystems.vision.VisionConstants;
-import java.util.List; 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import frc.robot.Constants.VisionConstants;
+import frc.robot.subsystems.vision.util.VisionResult;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-
 import org.littletonrobotics.junction.Logger;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
@@ -16,11 +17,7 @@ import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
 import org.photonvision.targeting.PhotonPipelineResult;
-
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import frc.robot.subsystems.vision.util.VisionResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 /** Add your docs here. */
 public class VisionIO_SIM implements VisionIO {
@@ -32,21 +29,22 @@ public class VisionIO_SIM implements VisionIO {
     VisionIOInputsAutoLogged inputs;
 
     public VisionIO_SIM() {
+        visionSim = new VisionSystemSim("VisionSim");
+        visionSim.addAprilTags(AprilTagFieldLayout.loadField(VisionConstants.field));
+
         SimCameraProperties camProperties = new SimCameraProperties();
         camProperties.setCalibration(320, 320, Rotation2d.fromDegrees(70));
-        // camProperties.setCalibError(0.25, 0.08);
         camProperties.setFPS(90);
-        // camProperties.setAvgLatencyMs(35);
-        // camProperties.setLatencyStdDevMs(5);
+
         cameras = new PhotonCamera[VisionConstants.CameraIDs.length];
-        simCameras = new PhotonCameraSim[VisionConstants.CameraIDs.length];
+        simCameras = new PhotonCameraSim[cameras.length];
         poseEstimators = new PhotonPoseEstimator[cameras.length];
-        visionSim = new VisionSystemSim("visionSim");
-        visionSim.addAprilTags(AprilTagFieldLayout.loadField(VisionConstants.field));
-        for (int i=0; i<cameras.length; i++) {
+
+        for (int i = 0; i < cameras.length; i++) {
             cameras[i] = new PhotonCamera(VisionConstants.CameraIDs[i]);
             simCameras[i] = new PhotonCameraSim(cameras[i], camProperties);
             poseEstimators[i] = new PhotonPoseEstimator(AprilTagFieldLayout.loadField(VisionConstants.field), VisionConstants.strategy, VisionConstants.CameraTransforms[i]);
+
             visionSim.addCamera(simCameras[i], poseEstimators[i].getRobotToCameraTransform());
         }
 
@@ -56,30 +54,41 @@ public class VisionIO_SIM implements VisionIO {
     @Override
     public void updateInputs() {
         inputs.unreadResults = getUnreadResults();
-
-        Logger.processInputs("/Vision/Cameras", inputs);
     }
 
     @Override
     public VisionResult[] getUnreadResults() {
-        Logger.recordOutput("Cameras/Measuring", true);
-        VisionResult[] visionMeasurements = new VisionResult[simCameras.length];
-        for (int i=0; i<simCameras.length; i++) {
-            List<PhotonPipelineResult> results = simCameras[i].getCamera().getAllUnreadResults();
-            if (results.size() > 0) {
-                Optional<EstimatedRobotPose> estimatedPose = poseEstimators[i].update(results.get(0));
-                if (estimatedPose.isPresent()) {
-                    visionMeasurements[i] = new VisionResult(estimatedPose.get().estimatedPose, results.get(0).getTimestampSeconds());
+        ArrayList<VisionResult> results = new ArrayList<VisionResult>();
+
+        for (int i = 0; i < simCameras.length; i++) {
+            PhotonCamera camera = simCameras[i].getCamera();
+            PhotonPoseEstimator poseEstimator = poseEstimators[i];
+
+            List<PhotonPipelineResult> pipelineResults = camera.getAllUnreadResults();
+            for (PhotonPipelineResult pipelineResult : pipelineResults) {
+                Optional<EstimatedRobotPose> pose = poseEstimator.update(pipelineResult);
+                
+                if (pose.isPresent()) {
+                    results.add(new VisionResult(pose.get().estimatedPose, pose.get().timestampSeconds));
                 }
+
+                List<PhotonTrackedTarget> targets = pipelineResult.getTargets();
+                int[] targetIds = new int[targets.size()];
+                for (int j = 0; j < targets.size(); j++) {
+                    PhotonTrackedTarget target = targets.get(i);
+
+                    int id = target.getFiducialId();
+                    targetIds[i] = id;
+
+                    Logger.recordOutput(String.format("/Vision/Targets/Target%d/Rotation", id), new Rotation3d(target.getSkew(), target.getPitch(), target.getYaw()));
+                    Logger.recordOutput(String.format("/Vision/Targets/Target%d/Transform", id), target.getBestCameraToTarget());
+                }
+
+                Logger.recordOutput("/Vision/TrackedIDs", targetIds);
             }
-            // Below is what we should use a fallback code otherwise use the code above unless it is absolutely necessary to use this code
-            // PhotonPipelineResult pipelineResult = simCameras[i].getCamera().getLatestResult();
-            // Optional<EstimatedRobotPose> pose = poseEstimators[i].update(pipelineResult);
-            // if (pose.isPresent()) {
-            //     visionMeasurements[i] = new VisionResult(pose.get().estimatedPose, pose.get().timestampSeconds);
-            // }
-            }
-        return visionMeasurements;
+        }
+
+        return (VisionResult[]) results.toArray();
     }
 
 	@Override
